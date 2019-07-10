98dc2876a44babc47abcc056e25ecbd6f13ad63e
minnja6 <jasmine.minniweather@verizon.com>
2019-07-10 12:35:35 -0500

    updates
    added code to game, phrase and app.js

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,17 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Game.js */
+class Game{
+    constructor(){
+       this.missed = 0;
+       this.phrases = [
+          new Phrase('Hit the nail on the head'),
+          new Phrase('You can’t judge a book by its cover'),
+          new Phrase('To kill two birds with one stone'),
+          new Phrase('When life gives you lemons, make lemonade'),
+          new Phrase('Dance like no one is watching')
+       ];
+       this.activePhrase = null;
+    }
+    getRandomPhrase() {
+        const index = Math.floor(Math.random() * this.phrase.length);
+        return this.phrase[index];
+    }
+};
